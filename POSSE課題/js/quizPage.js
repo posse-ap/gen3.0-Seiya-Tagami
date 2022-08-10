a54f2fe0780347dc8935861f897a6c9ff3a33c7f
Seiya-Tagami <bepozurapu274@gmail.com>
2022-08-10 18:06:17 +0900

    残りの必要箇所のクラス名を変更

--- POSSE課題/js/quizPage.js
+++ POSSE課題/js/quizPage.js
@@ -1,7 +1,7 @@
 'use strict'
 
 { 
-  const quizSet = shuffle([
+  const quizSet = [
     {q:"日本のIT人材が2030年には最大どれくらい不足すると言われているでしょうか？",o:['約28万人','約79万人','約11倍'],h:"../img/quiz/img-quiz01.png",d:'経済産業省 2019年3月 － IT 人材需給に関する調査',dsrc:'https://www.meti.go.jp/policy/it_policy/jinzai/houkokusyo.pdf'},
     {q:'既存業界のビジネスと、先進的なテクノロジーを結びつけて生まれた、新しいビジネスのことをなんと言うでしょう？',o:['X-TECH','INTECH','BIZZTECH'],h:"../img/quiz/img-quiz02.png",d:'',dsrc:''},
     {q:'IoTとは何の略でしょう？',o:['Internet of Things','Integrate into Technology','Information  on Tool'],h:"../img/quiz/img-quiz03.png",d:'',dsrc:''},
@@ -9,17 +9,18 @@
     {q:' イギリスのコンピューター科学者であるギャビン・ウッド氏が提唱した、ブロックチェーン技術を活用した「次世代分散型インターネット」のことをなんと言うでしょう？',o:['Web3.0','NFT','メタバース'],h:'../img/quiz/img-quiz05.png',d:'',dsrc:''},
     {q:'先進テクノロジー活用企業と出遅れた企業の収益性の差はどれくらいあると言われているでしょうか？',o:['約5倍','約2倍','約11倍'],h:'../img/quiz/img-quiz06.png',d:'Accenture Technology Vision 2021 ',dsrc:'https://www.accenture.com/jp-ja/insights/technology/technology-trends-2022'},
     {q:'法律とITが組み合わさった新たな仕事の領域をなんというか',o:['リーガルテック','ローテック','アイティーリーガル'],h:'https://wealthroad.jp/wp-content/uploads/2019/09/11shutterstock_1079923613.jpg',d:'法律×IT「リーガルテック」で何がどう変わりつつあるのか？',dsrc:'https://wealthroad.jp/archives/157'}
-  ]);
+  ];
   
-
   function shuffle(arr) {
     for(let i = arr.length-1; i>0;i--){
     const j = Math.floor(Math.random()*(i+1));
     [arr[j],arr[i]]=[arr[i],arr[j]];
   }
   return arr;
-}
+}  //シャッフル
 
+  const shuffledQuizset = shuffle(quizSet);  //quizSetをシャッフル
+  
   const showTrue = document.getElementsByClassName('p_quiz_answer_t');
   const showFalse = document.getElementsByClassName('p_quiz_answer_f');
   const optBox = document.getElementsByClassName('p_quiz_optbox');
@@ -43,25 +44,25 @@
       Section[currentNum].appendChild(createQnum);
       createQnum.classList.add('p_quiz_qnum') //設問ナンバーを生成 
 
-      const createQ = document.createElement('div');
-      Section[currentNum].appendChild(createQ);
-      createQ.classList.add('p_quiz_q') //設問の大枠を生成
+      const createQuestion = document.createElement('div');
+      Section[currentNum].appendChild(createQuestion);
+      createQuestion.classList.add('p_quiz_q') //設問の大枠を生成
 
       const makeQuiz = document.getElementsByClassName('p_quiz_q');
       const createQuiztxt = document.createElement('span');
-      createQuiztxt.textContent = quizSet[currentNum].q;
+      createQuiztxt.textContent = shuffledQuizset[currentNum].q;
       makeQuiz[currentNum].appendChild(createQuiztxt);
       createQuiztxt.classList.add('p_quiz_qtxt');//設問を生成
       
       const createImg = document.createElement('img');
-      createImg.src = quizSet[currentNum].h;
+      createImg.src = shuffledQuizset[currentNum].h;
       makeQuiz[currentNum].appendChild(createImg);
       createImg.classList.add('p_quiz_qimg');//設問の画像を生成
 
-      const createA = document.createElement('div');
-      Section[currentNum].appendChild(createA);
-      createA.textContent = 'A';
-      createA.classList.add('p_quiz_a');//アンサーのアルファベット生成
+      const createAnsweralphabet = document.createElement('div');
+      Section[currentNum].appendChild(createAnsweralphabet);
+      createAnsweralphabet.textContent = 'A';
+      createAnsweralphabet.classList.add('p_quiz_a');//アンサーのアルファベット生成
 
       const createQuizbox = document.createElement('div');
       Section[currentNum].appendChild(createQuizbox);
@@ -72,12 +73,12 @@
       quizBox[currentNum].appendChild(createOptbox);
       createOptbox.classList.add('p_quiz_optbox');//選択肢の箱生成
       
-      const shuffledChoices = shuffle([...quizSet[currentNum].o]);
+      const shuffledChoices = shuffle([...shuffledQuizset[currentNum].o]);
       shuffledChoices.forEach(option =>{
-      const createli = document.createElement('li');
-      createli.textContent = option;
-      optBox[currentNum].appendChild(createli);
-      createli.classList.add('p_quiz_option','p_quiz_option_disappear')
+      const createOption = document.createElement('li');
+      createOption.textContent = option;
+      optBox[currentNum].appendChild(createOption);
+      createOption.classList.add('p_quiz_option','p_quiz_option_disappear')
       });//選択肢生成
 
       const createTruebox = document.createElement('div');
@@ -94,7 +95,7 @@
       const showTrue = document.getElementsByClassName('p_quiz_answer_t');
       createTrue.textContent = '正解！'
       createSpan01.textContent = 'A';
-      createspan02.textContent = `${quizSet[currentNum].o[0]}`;
+      createspan02.textContent = `${shuffledQuizset[currentNum].o[0]}`;
       showTrue[currentNum].appendChild(createTrue);
       showTrue[currentNum].appendChild(createSpan01);
       showTrue[currentNum].appendChild(createspan02);
@@ -107,24 +108,24 @@
       const showFalse = document.getElementsByClassName('p_quiz_answer_f');
       createFalse.textContent = '不正解...';
       createSpan03.textContent = 'A';
-      createSpan04.textContent = `${quizSet[currentNum].o[0]}`;
+      createSpan04.textContent = `${shuffledQuizset[currentNum].o[0]}`;
       showFalse[currentNum].appendChild(createFalse);
       showFalse[currentNum].appendChild(createSpan03);
       showFalse[currentNum].appendChild(createSpan04);
       createSpan03.classList.add('p_quiz_answer_txt_01');
       createSpan04.classList.add('p_quiz_answer_txt_02'); //正解不正解の内容を生成 
       
-      if(quizSet[currentNum].d === ''){
+      if(shuffledQuizset[currentNum].d === ''){
         return;
       } else {
         const createDetail = document.createElement('a');
           quizBox[currentNum].appendChild(createDetail);
-          createDetail.setAttribute('href',`${quizSet[currentNum].dsrc}`);
+          createDetail.setAttribute('href',`${shuffledQuizset[currentNum].dsrc}`);
           createDetail.classList.add('p_quiz_art');
     
           const quizArt = document.getElementsByClassName('p_quiz_art');
           const createDetailtxt = document.createElement('p');
-          createDetailtxt.textContent = quizSet[currentNum].d;
+          createDetailtxt.textContent = shuffledQuizset[currentNum].d;
     
           for(let d = 0; d < quizArt.length; d++){
             quizArt[d].appendChild(createDetailtxt);
