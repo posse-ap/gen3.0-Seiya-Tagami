5eb85f14ba18971cf09d064e797054efa61a188b
Seiya-Tagami <bepozurapu274@gmail.com>
2022-08-12 10:07:58 +0900

    トップページのレスポンシブ対応、ハンバーガーメニュー完成

---
+++ POSSE課題/js/common.js
@@ -0,0 +1,12 @@
+const hamburgurMenu = document.getElementById('hamburger_menu');
+hamburgurMenu.addEventListener('click',() =>{
+  const clickBefore = document.getElementsByClassName('hamburger_menu_clickbefore');
+  clickBefore[0].classList.toggle('hamburger_menu_clickafter');
+
+  const clickAfter = document.getElementsByClassName('p_header_ul');
+  clickAfter[0].classList.toggle('p_header_ul_clickafter');
+
+  const fotterLine = document.getElementsByClassName('p_footter_line');
+  fotterLine[0].classList.toggle('p_footter_line_disappear');
+
+})
