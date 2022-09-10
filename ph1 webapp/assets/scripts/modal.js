0db1f59d0b2f932db24df8b4a768f3a9caa4aa70
Seiya-Tagami <bepozurapu274@gmail.com>
2022-09-10 11:48:40 +0900

    午前中の作業終了

--- ph1 webapp/assets/scripts/modal.js
+++ ph1 webapp/assets/scripts/modal.js
@@ -1,12 +1,20 @@
-'use strict'
+"use strict";
 
 {
-  const modal = document.querySelector('.js-modal-open');
-  document.querySelector('.js-record').addEventListener('click', ()=> {
-    modal.classList.add('u-display-block');
-  })
+  const modal = document.querySelector(".js-modal-open");
+  const modalInner = document.querySelector(".js-modal-inner");
 
-  document.querySelector('.js-modal-close').addEventListener('click', ()=> {
-    modal.classList.remove('u-display-block');
-  })
+  document.querySelector(".js-record").addEventListener("click", () => {
+    modal.classList.toggle("u-display-block");
+    modalInner.classList.remove("u-display-hidden");
+    //改良の余地あり
+  });
+
+  document.querySelector(".js-modal-close").addEventListener("click", () => {
+    modal.classList.toggle("u-display-block");
+  });
+
+  document.querySelector(".js-record-is-done").addEventListener("click", () => {
+    modalInner.classList.add("u-display-hidden");
+  });
 }
