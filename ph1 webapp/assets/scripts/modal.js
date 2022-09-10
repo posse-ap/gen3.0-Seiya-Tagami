0186f223a6f3d1bf4f6d510928c48ff945da3fb1
Seiya-Tagami <bepozurapu274@gmail.com>
2022-09-10 14:51:36 +0900

    投稿完了画面等実装完了

--- ph1 webapp/assets/scripts/modal.js
+++ ph1 webapp/assets/scripts/modal.js
@@ -3,18 +3,40 @@
 {
   const modal = document.querySelector(".js-modal-open");
   const modalInner = document.querySelector(".js-modal-inner");
+  const recordDone = document.querySelector(".js-record-done");
+  const checkBoxes = document.querySelectorAll(".js-checkbox");
+  const writtenTexts = document.querySelectorAll(".js-written-text");
+
+  const unChecked = (checkBoxes) => {
+    checkBoxes.forEach((checkbox) => {
+      checkbox.checked = false;
+    });
+  };
+
+  const clearText = (writtenTexts) => {
+    writtenTexts.forEach((text)=>{
+      text.value = "";
+    })
+  }
 
   document.querySelector(".js-record").addEventListener("click", () => {
     modal.classList.toggle("u-display-block");
-    modalInner.classList.remove("u-display-hidden");
     //改良の余地あり
   });
 
   document.querySelector(".js-modal-close").addEventListener("click", () => {
     modal.classList.toggle("u-display-block");
+    modalInner.classList.remove("u-display-hidden");
+    recordDone.classList.remove("u-display-flex");
+    unChecked(checkBoxes);
+    clearText(writtenTexts);
+    //改良の余地あり
   });
 
-  document.querySelector(".js-record-is-done").addEventListener("click", () => {
-    modalInner.classList.add("u-display-hidden");
-  });
+  document
+    .querySelector(".js-button-record-done")
+    .addEventListener("click", () => {
+      modalInner.classList.add("u-display-hidden");
+      recordDone.classList.add("u-display-flex");
+    });
 }
