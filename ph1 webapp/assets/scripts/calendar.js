2e8f0cad1f2f354da18a4ac8b0fcae693507d6ed
Seiya-Tagami <bepozurapu274@gmail.com>
2022-09-14 12:16:44 +0900

    feat: カレンダーの基本的な機能実装

--- ph1 webapp/assets/scripts/calendar.js
+++ ph1 webapp/assets/scripts/calendar.js
@@ -1,9 +1,6 @@
 "use strict";
 
 {
-  (function(){
-
-  
   const today = new Date();
   let year = today.getFullYear();
   let month = today.getMonth();
@@ -11,11 +8,11 @@
   const getCalendarHead = () => {
     const dates = [];
     const d = new Date(year, month, 0).getDate();
-    const n = new Date(year, month, 1).getDay(); 
+    const n = new Date(year, month, 1).getDay();
     //曜日を取得
     //memo: 0は日曜日、1は月曜日と続く
 
-    for ( let i = 0; i < n; i++) {
+    for (let i = 0; i < n; i++) {
       dates.unshift({
         date: d - i,
         isToday: false,
@@ -23,7 +20,7 @@
       });
     }
     return dates;
-  }
+  };
 
   const getCalenderBody = () => {
     const dates = [];
@@ -34,73 +31,54 @@
         date: i,
         isToday: false,
         isDisabled: false,
-        isFutureDay:false
       });
     }
 
-    if(year === today.getFullYear() && month == today.getMonth()){
+    if (year === today.getFullYear() && month == today.getMonth()) {
       dates[today.getDate() - 1].isToday = true;
     }
 
     return dates;
   };
 
-  const getCalendarTail = () => {
-    const dates = [];
-    const lastDay =  new Date(year, month + 1, 0).getDay();
-
-    for ( let i = 1; i < 7 - lastDay; i++) {
-      dates.push({
-        date: i,
-        isToday: false,
-        isDisabled: true,
-      });
-    }
-    return dates;
-  }
-
   const clearCalendar = () => {
-    const tbody = document.querySelector('tbody');
+    const tbody = document.querySelector("tbody");
     tbody.innerHTML = "";
-  } //カレンダーの中身を削除
+  }; //カレンダーの中身を削除
 
   const renderTitle = () => {
-    const title = `${year}年${String(month + 1).padStart(2, '0')}月`;
-    document.getElementById('js-title').textContent = title;
-  }
+    const title = `${year}年${String(month + 1).padStart(2, "0")}月`;
+    document.getElementById("js-title").textContent = title;
+  };
 
   const renderWeeks = () => {
-    const dates = [
-      ...getCalendarHead(),
-      ...getCalenderBody(),
-      ...getCalendarTail(),
-    ];
+    const dates = [...getCalendarHead(), ...getCalenderBody()];
 
     const weeks = [];
-    const weekCount = dates.length/7;
+    const weekCount = dates.length / 7;
 
-    for(let i = 0; i < weekCount; i++){
-      weeks.push(dates.splice(0,7))
+    for (let i = 0; i < weekCount; i++) {
+      weeks.push(dates.splice(0, 7));
     }
 
-    weeks.forEach(week => {
-      const tr = document.createElement('tr');
-      week.forEach(date => {
-        const td = document.createElement('td');
+    weeks.forEach((week) => {
+      const tr = document.createElement("tr");
+      week.forEach((date) => {
+        const td = document.createElement("td");
 
         td.textContent = date.date;
-        if (date.isToday){
-          td.classList.add('today');
+        if (date.isToday) {
+          td.classList.add("u-colors-calendar-pushed");
         }
         if (date.isDisabled) {
-          td.classList.add('disabled');
+          td.classList.add("disabled");
         }
 
         tr.appendChild(td);
       });
-      document.querySelector('tbody').appendChild(tr);
-    })
-  }
+      document.querySelector("tbody").appendChild(tr);
+    });
+  };
 
   function createCalendar() {
     clearCalendar();
@@ -108,36 +86,75 @@
     renderWeeks();
   }
 
-  document.getElementById('js-prev').addEventListener('click', ()=>{
+  document.getElementById("js-prev").addEventListener("click", () => {
     month--;
     if (month < 0) {
       year--;
-      month = 11; 
+      month = 11;
     }
-
+    setToday();
     createCalendar();
-  });
+  }); //前の月へ
 
-  document.getElementById('js-next').addEventListener('click', ()=>{
+  document.getElementById("js-next").addEventListener("click", () => {
     month++;
     if (month > 11) {
       year++;
-      month = 0; 
-    }
+      month = 0;
+    } //次の月へ
 
+    setToday();
     createCalendar();
   });
-
   createCalendar();
-})();
 
-  const calenderDates = document.querySelectorAll('td');
-  let dateInner;
-  calenderDates.forEach((date)=>{
-    date.addEventListener('click', () =>{
-      dateInner = date.textContent;
-      console.log(dateInner);
-    })
-  })
-  //eを使うべきかもしれない、それでtargetとかやるやつ。querySelectorだと全部取得できない
+  const studyingDate = document.getElementById("js-studying-date");
+  const dates = document.getElementById("js-dates");
+  let dateInner = `${year}年${String(month + 1).padStart(2,"0")}月${today.getDate()}日`;
+  
+  const initializeDate = () => {
+    studyingDate.value = `${year}年${String(month + 1).padStart(
+      2,
+      "0"
+    )}月${today.getDate()}日`;
+  }// 初期値は今日の日付にする
+  initializeDate();
+
+  const setToday = () => {
+    if (year === today.getFullYear() && month == today.getMonth()) {
+      dateInner = `${year}年${String(month + 1).padStart(
+        2,
+        "0"
+      )}月${today.getDate()}日`;
+    }
+  } //今月に戻ったら、強制的に今日の日付を指すようにする
+
+  dates.addEventListener("click", (e) => {
+    if (e.target.nodeName === "TD") {
+      const allDates = dates.querySelectorAll("td");
+      allDates.forEach((date) => {
+        date.classList.remove("u-colors-calendar-pushed");
+      });
+
+      dateInner = `${year}年${String(month + 1).padStart(2, "0")}月${
+        e.target.textContent
+      }日`; //dateInnerに日付を格納
+      e.target.classList.add("u-colors-calendar-pushed");
+    }
+  });  
+
+    const modalInner = document.querySelector(".js-modal-inner");
+    const modalClose = document.querySelector(".js-modal-close");
+    const modalBack = document.querySelector(".js-modal-back");
+    const calendar = document.querySelector(".js-calendar");
+    const calendarButton = document.querySelector(".js-calendar-button");
+    //memo 煩雑だからなんとかしたい
+
+    calendarButton.addEventListener('click', () => {
+      studyingDate.value = dateInner;
+      modalClose.classList.remove("u-display-hidden");
+      modalInner.classList.remove("u-display-hidden");
+      modalBack.classList.remove("u-display-block");
+      calendar.classList.remove("u-display-block");
+    });
 }
