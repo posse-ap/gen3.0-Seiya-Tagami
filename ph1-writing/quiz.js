15930b900ebc6c4679a253e796885331f15ad25d
Seiya-Tagami <bepozurapu274@gmail.com>
2022-09-24 18:43:23 +0900

    refactor: jsコード一部改善

--- ph1-writing/quiz.js
+++ ph1-writing/quiz.js
@@ -54,40 +54,44 @@
 
   const shuffledQuiz = shuffleArray(ALL_QUIZ);
   const quizArea = document.getElementById('js-quiz-area');
+  
+  function createQuiz(){
+    shuffledQuiz.forEach((quiz, index)=> {
+      const shuffleChoices = shuffleArray(quiz.choices);
+      const quoteHtml = quiz.quote ? `<cite>${quiz.quote}</cite>` : "";
+      const quizHtml = `<div class="p-quiz js-quiz">
+      <div class="p-quiz__header"> 
+        <div class="p-quiz__header__quizlabel">Q${index + 1}</div>
+        <span class="p-quiz__header__question">${quiz.question}</span>
+        <div class="p-quiz__header__image"><img src="${quiz.img}" alt="" /></div>
+      </div>
+      <div class="p-quiz__answerlabel">A</div>
+      <div class="p-quiz__answer-box">
+        <ul class="p-quiz__answer-box__choices">
+          <li><button class="p-quiz__answer-box__choices__button is-attached-arrow js-answer">${shuffleChoices[0]}</button></li>
+          <li><button class="p-quiz__answer-box__choices__button is-attached-arrow js-answer">${shuffleChoices[1]}</button></li>
+          <li><button class="p-quiz__answer-box__choices__button is-attached-arrow js-answer">${shuffleChoices[2]}</button></li>
+        </ul>
+        <div class="p-quiz__answer-box__answer-true js-true">
+          <div class="p-quiz__answer-box__answer-true__textbox">
+            <span>正解！</span>
+            <div><span>A</span><span>${quiz.choices[quiz.correctnum]}</span></div>
+          </div>
+        </div>
+        <div class="p-quiz__answer-box__answer-false js-false">
+          <div class="p-quiz__answer-box__answer-false__textbox">
+            <span>不正解...</span>
+            <div><span>A</span><span>${quiz.choices[quiz.correctnum]}</span></div>
+          </div>
+        </div>
+        ${quoteHtml}
+      </div>
+    </div>`;
+    quizArea.insertAdjacentHTML('beforeend', quizHtml);
+    });
+  }
+  createQuiz()
 
-  shuffledQuiz.forEach((quiz, index)=> {
-    const shuffleChoices = shuffleArray(quiz.choices);
-    const quoteHtml = quiz.quote ? `<cite>${quiz.quote}</cite>` : "";
-    const quizHtml = `<div class="p-quiz js-quiz">
-    <div class="p-quiz__header"> 
-      <div class="p-quiz__header__quizlabel">Q${index + 1}</div>
-      <span class="p-quiz__header__question">${quiz.question}</span>
-      <div class="p-quiz__header__image"><img src="${quiz.img}" alt="" /></div>
-    </div>
-    <div class="p-quiz__answerlabel">A</div>
-    <div class="p-quiz__answer-box">
-      <ul class="p-quiz__answer-box__choices">
-        <li><button class="p-quiz__answer-box__choices__button is-attached-arrow js-answer">${shuffleChoices[0]}</button></li>
-        <li><button class="p-quiz__answer-box__choices__button is-attached-arrow js-answer">${shuffleChoices[1]}</button></li>
-        <li><button class="p-quiz__answer-box__choices__button is-attached-arrow js-answer">${shuffleChoices[2]}</button></li>
-      </ul>
-      <div class="p-quiz__answer-box__answer-true js-true">
-        <div class="p-quiz__answer-box__answer-true__textbox">
-          <span>正解！</span>
-          <div><span>A</span><span>${quiz.choices[quiz.correctnum]}</span></div>
-        </div>
-      </div>
-      <div class="p-quiz__answer-box__answer-false js-false">
-        <div class="p-quiz__answer-box__answer-false__textbox">
-          <span>不正解...</span>
-          <div><span>A</span><span>${quiz.choices[quiz.correctnum]}</span></div>
-        </div>
-      </div>
-      ${quoteHtml}
-    </div>
-  </div>`;
-  quizArea.insertAdjacentHTML('beforeend', quizHtml);
-  });
 
   const allQuiz = document.querySelectorAll('.js-quiz');
 
@@ -95,31 +99,35 @@
     answers.forEach(answer => {
       answer.disabled = true;
     })
-  }
+  } //ボタン非活性化
 
   const removeArrows = (answers) => {
     answers.forEach(answer => {
       answer.classList.remove('is-attached-arrow')
     })
-  }
+  } //ボタンの三角アイコンを外す
 
-  allQuiz.forEach((quiz, index)=> {
-    const answers = quiz.querySelectorAll('.js-answer');
-    const answerTrue = quiz.querySelector('.js-true');
-    const answerFalse = quiz.querySelector('.js-false');
-    const correctAnswer = shuffledQuiz[index].choices[shuffledQuiz[index].correctnum];
-
-    answers.forEach((answer)=>{
-      answer.addEventListener('click', () => {
-        setDisabled(answers);
-        removeArrows(answers);
-        answer.classList.add('is-selected');
-        if(answer.textContent === correctAnswer){
-          answerTrue.classList.add('block');
-        } else {
-          answerFalse.classList.add('block');
-        }
+  function checkAnswer(){
+    allQuiz.forEach((quiz, index) => {
+      const answers = quiz.querySelectorAll('.js-answer');
+      const answerTrue = quiz.querySelector('.js-true');
+      const answerFalse = quiz.querySelector('.js-false');
+      const correctNum = shuffledQuiz[index].correctnum;
+      const correctAnswer = shuffledQuiz[index].choices[correctNum];
+  
+      answers.forEach((answer)=>{
+        answer.addEventListener('click', () => {
+          setDisabled(answers);
+          removeArrows(answers);
+          answer.classList.add('is-selected');
+          if(answer.textContent === correctAnswer){
+            answerTrue.classList.add('block');
+          } else {
+            answerFalse.classList.add('block');
+          }
+        })
       })
     })
-  })
+  }
+  checkAnswer();
 }
