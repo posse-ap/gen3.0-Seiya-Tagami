a57ee2cd22f3d77a5bdc6a35a48ade160bcc144d
Seiya-Tagami <bepozurapu274@gmail.com>
2022-09-25 11:57:53 +0900

    feat: modalの提出時のvalidation機能追加

--- ph1 webapp/assets/scripts/modal.js
+++ ph1 webapp/assets/scripts/modal.js
@@ -6,7 +6,7 @@
   const nowLoading = document.querySelector(".js-now-loading");
   const recordDone = document.querySelector(".js-record-done");
   const checkBoxes = document.querySelectorAll(".js-checkbox");
-  const inputedTexts = document.querySelectorAll(".js-inputed-text");
+  const inputTexts = document.querySelectorAll(".js-input-text");
 
   const unChecked = (checkBoxes) => {
     checkBoxes.forEach((checkbox) => {
@@ -14,24 +14,67 @@
     });
   }; //チェックを全て外す
 
-  const clearText = (inputedTexts) => {
-    inputedTexts.forEach((text) => {
-      text.value = "";
+  const clearText = (inputTexts) => {
+    inputTexts.forEach((inputText) => {
+      inputText.value = "";
     });
   }; //テキストを入力前の状態にする
 
+
+  let allowedRecord;
+  const regulationAlerts = document.querySelectorAll('.js-alert');
+
+  const clearRegulationAlerts = (regulationAlerts) => {
+    regulationAlerts.forEach((regulationAlert) =>{
+      regulationAlert.classList.remove('u-display-block')
+    })
+  }; //アラート表示をなくす
+
+  const checkInputThings = () => {
+    clearRegulationAlerts(regulationAlerts);
+    const ContentsCheckBoxes = document.querySelectorAll(".js-contents-checkbox");
+    const LanguagesCheckBoxes = document.querySelectorAll(".js-languages-checkbox");
+    const studyingTime = document.querySelector(".js-studying-time");
+    let ContentsCount = 0;
+    let LanguagesCount = 0;
+
+    for (let i = 0; i < ContentsCheckBoxes.length; i++) {
+      if (ContentsCheckBoxes[i].checked) {
+        ContentsCount++;
+      }
+    }
+    for (let i = 0; i < LanguagesCheckBoxes.length; i++) {
+      if (LanguagesCheckBoxes[i].checked) {
+        LanguagesCount++;
+      }
+    }
+    if (ContentsCount > 0 && LanguagesCount > 0 && studyingTime.value.length !== 0) {
+      allowedRecord = true;
+    } else {
+      allowedRecord = false;
+      if(ContentsCount <= 0){
+        regulationAlerts[0].classList.add('u-display-block');
+      }
+      if(LanguagesCount <= 0){
+        regulationAlerts[1].classList.add('u-display-block');
+      }
+      if(studyingTime.value.length === 0){
+        regulationAlerts[2].classList.add('u-display-block');
+      }
+    }
+  }; //validation
+
   const tweetArea = document.getElementById("js-tweetarea");
-  const regulationAlert = document.querySelector(".js-alert");
   const shareTweet = document.getElementById("js-tweet-share");
 
   const countLetters = () => {
     tweetArea.addEventListener("input", (e) => {
       if (e.target.value.length > 140) {
-        regulationAlert.classList.add("u-display-block");
+        regulationAlerts[3].classList.add("u-display-block");
         shareTweet.checked = false;
         shareTweet.disabled = true;
       } else if (e.target.value.length <= 140) {
-        regulationAlert.classList.remove("u-display-block");
+        regulationAlerts[3].classList.remove("u-display-block");
         shareTweet.disabled = false;
       }
     });
@@ -40,9 +83,7 @@
   const tweet = () => {
     if (shareTweet.checked) {
       const tweetText = `${tweetArea.value}`;
-      window.open(
-        `http://twitter.com/intent/tweet?&text=${tweetText}`
-      );
+      window.open(`http://twitter.com/intent/tweet?&text=${tweetText}`);
     }
   }; //ツイート機能
 
@@ -70,9 +111,9 @@
     modalInner.classList.remove("u-display-hidden");
     recordDone.classList.remove("u-display-flex");
     nowLoading.classList.remove("u-display-block");
-    regulationAlert.classList.remove("u-display-block");
+    clearRegulationAlerts(regulationAlerts);
     unChecked(checkBoxes);
-    clearText(inputedTexts);
+    clearText(inputTexts);
   }); //モーダルを閉じる
 
   const studyingDate = document.getElementById("js-studying-date");
@@ -89,6 +130,10 @@
   const record = document.querySelector(".js-button-record-done");
 
   record.addEventListener("click", () => {
+    checkInputThings();
+    if (allowedRecord === false) {
+      return;
+    }
     tweet();
     window.scroll({ top: 0, behavior: "smooth" });
     modalInner.classList.add("u-display-hidden");
