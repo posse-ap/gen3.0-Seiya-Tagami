71e14eba47e0641be441c64574aae325e1d43cf8
Seiya-Tagami <bepozurapu274@gmail.com>
2022-10-20 15:36:32 +0900

    テーブル追加で作成

--- ph2-website/src/quiz.php
+++ ph2-website/src/quiz.php
@@ -1,3 +1,22 @@
+<?php
+declare(strict_types=1);
+
+$dsn = 'mysql:host=db;dbname=quiz;charset=utf8mb4';
+$user = 'root';
+$password = 'root';
+
+try {
+  $pdo = new PDO($dsn, $user, $password, [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+  ]);
+} catch (PDOException $e) {
+  echo '接続失敗: ' . $e->getMessage();
+  exit();
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
   <head>
