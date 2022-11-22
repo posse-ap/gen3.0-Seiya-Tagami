0adb3b19372300d49a0d58087322b79dbaca8a34
Seiya-Tagami <bepozurapu274@gmail.com>
2022-11-22 17:18:07 +0900

    feat: フォルダ・ファイルを構成

---
+++ ph2-website/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors :{
+        header: "#F5F5F5",
+        blue: "#0071BC"
+      }
+    },
+  },
+  plugins: [],
+}
